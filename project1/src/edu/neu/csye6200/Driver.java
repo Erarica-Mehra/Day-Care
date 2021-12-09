3755a8b44b9445918ea4ee050f0ee259125ce31c
Erarica Mehra <erarica.mehra@gmail.com>
2021-12-09 17:57:31 -0500

    Fixed compilation errors

--- project1/src/edu/neu/csye6200/Driver.java
+++ project1/src/edu/neu/csye6200/Driver.java
@@ -3,6 +3,17 @@
  */
 package edu.neu.csye6200;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import edu.neu.csye6200.dao.ClassGroupDaoImpl;
+import edu.neu.csye6200.dao.FeedbackDaoImpl;
+import edu.neu.csye6200.dao.StudentDaoImpl;
+import edu.neu.csye6200.dao.TeacherDaoImpl;
+
 /**
  * @author pnakave
  *
@@ -11,12 +22,11 @@ public class Driver {
 
 	/**
 	 * @param args
+	 * @throws Exception 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 
-		StudentService service = new StudentService();
-		service.registerStudentFromCSV();
-		service.registerTeacherFromCSV();
+		
 	}
 
 }
